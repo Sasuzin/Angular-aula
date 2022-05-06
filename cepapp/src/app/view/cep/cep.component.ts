56a98f4c1f6bc701842ce8bf961a74165328543a
LUIS FELIPE GALVAO TORRADO <luis.fgtorrado@senacsp.edu.br>
2022-05-05 21:58:19 -0300

    Consulta de Cep feito

---
+++ cepapp/src/app/view/cep/cep.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Endereco } from 'src/app/Model/endereco';
+import { EnderecoService } from 'src/app/services/endereco.service';
+
+@Component({
+  selector: 'app-cep',
+  templateUrl: './cep.component.html',
+  styleUrls: ['./cep.component.css']
+})
+export class CepComponent implements OnInit {
+    cep ='';
+    endereco?: Endereco;
+
+  constructor(private endService: EnderecoService) { }
+
+  ngOnInit(): void {
+  }
+
+
+    consultarCep() {
+      this.endService.obterEndereco(this.cep).subscribe(endereco =>{
+        this.endereco = endereco;
+      });
+    }
+
+}
